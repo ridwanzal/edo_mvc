85b3f2932f6cd9a80393aac1c3c484f6ea1ffe35
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-04-17 10:38:29 +0700

    asset css

--- application/views/frontheader.php
+++ application/views/frontheader.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" href="<?php echo base_url() ?>assets/img/favicons.png"/>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet" type="text/css">
         <!-- <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"> -->
         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
