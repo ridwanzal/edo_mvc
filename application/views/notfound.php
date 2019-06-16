c2aefe2ede894a505cc3f54c4ad4da21e4293515
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-06-16 12:23:08 +0700

    notfound bar baru

--- application/views/notfound.php
+++ application/views/notfound.php
@@ -1,8 +1,10 @@
 <div class="thirdsection">
-    <div class="container" style="padding-top:12%;padding-bottom:13%;">
+    <div class="container" style="padding-top:10%;padding-bottom:11%;">
         <div class="row">
-            <div class="col-lg-12 col-md-6 col-xs-12">
-                <center><h1 style='font-weight:bold;letter-spacing:-0.5px;'>Sorry, page not found</h1></center>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <center><h1 style='font-weight:bold;letter-spacing:-0.5px; text-align:left;font-size:60px;'>404</h1></center>
+                <center><p style='font-weight:bold;letter-spacing:-0.5px;text-align:left;'>Maaf, halaman yang anda tuju tidak ditemukan (error 404).</p></center>
+                <center><p style='font-weight:lighter;text-align:left;'>Periksa kembali apakah alamat URL yang Anda masukkan sudah benar.</p></center>
             </div>
         </div>
     </div>
