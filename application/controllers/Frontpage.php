740179faf446da223f844d11584ba85a86239082
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-06-17 13:53:49 +0700

    remove mail conf

--- application/controllers/Frontpage.php
+++ application/controllers/Frontpage.php
@@ -67,43 +67,6 @@ class Frontpage extends CI_Controller {
     $this->db->insert('contact', $data);
     $check = $this->db->affected_rows() > 0;
 		if($check){
-      $configs = [
-        'mailtype'  => 'html',
-        'charset'   => 'utf-8',
-        'protocol'  => 'smtp',
-        'smtp_host' => 'ssl://smtp.gmail.com',
-        'smtp_user' => 'zalbinaridwan@gmail.com',    // Ganti dengan email gmail kamu
-        'smtp_pass' => 'indrabesmara933993',      // Password gmail kamu
-        'smtp_port' => 465,
-        'crlf'      => "\r\n",
-        'newline'   => "\r\n"
-      ];
-
-      // Load library email dan konfigurasinya
-      $this->load->library('email', $configs);
-
-      // Email dan nama pengirim
-      $this->email->from('zalbinaridwan@gmail.com', 'MRZ');
-
-      // Email penerima
-      $this->email->to('mridwanzalbina@gmail.com'); // Ganti dengan email tujuan kamu
-
-      // Lampiran email, isi dengan url/path file
-      $this->email->attach('https://masrud.com/content/images/20181215150137-codeigniter-smtp-gmail.png');
-
-      // Subject email
-      $this->email->subject('Kirim Email dengan SMTP Gmail | MasRud.com');
-
-      // Isi email
-      $this->email->message("Ini adalah contoh email CodeIgniter yang dikirim menggunakan SMTP email Google (Gmail).<br><br> Klik <strong><a href='https://masrud.com/post/kirim-email-dengan-smtp-gmail' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
-
-      // Tampilkan pesan sukses atau error
-      if ($this->email->send()) {
-          echo 'Sukses! email berhasil dikirim.';
-        $this->session->set_flashdata('success', 'Submit data berhasil, Terima kasih.');
-      } else {
-          echo 'Error! email tidak dapat dikirim.';
-      }
       $this->session->set_flashdata('success', 'Submit data berhasil, Terima kasih.');
 			redirect(base_url('frontpage/contact'));
 		}
