a548f9e921cb4fef0b05636002b00f1cc25cf616
ridwanzal <zalbinaridwan@gmail.com>
2020-02-24 11:32:15 +0700

    add all

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,7 +219,7 @@ $(document).ready(function () {
             let elem = $('#instacontainer');
             let i = 0;
             for (i; i < res.data.length; i++) {
-                limit = 18;
+                limit = 100;
                 if (i < limit) {
                     let img_uri = res.data[i].images.standard_resolution.url;
                     let object = '<div class="col-lg-2 col-md-2 col-xs-4 item container_mag2 animated fadeIn"><a href=' + img_uri + '><img src=' + img_uri + ' style="margin-top:3px; margin-bottom:3px;width:190px; border:1px solid #eee;" class="img_insta"></a></div>';
