e8262798f52b414cd2967129cd555ddd12d08a33
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2020-05-30 08:00:34 +0700

    perubahan baru

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,7 +222,7 @@ $(document).ready(function () {
                 limit = 12;
                 if (i < limit) {
                     let img_uri = res.data[i].images.standard_resolution.url;
-                    let object = '<div class="col-lg-4 col-md-4 col-xs-4 item container_mag2 animated fadeIn"><a href=' + img_uri + '><img src=' + img_uri + ' style="width:100%;margin-bottom:20px;border:1px solid #eee;" class="img_insta"></a></div>';
+                    let object = '<div class="col-lg-4 col-md-4 col-xs-6 col-sm-6 item container_mag2 animated fadeIn"><a href=' + img_uri + '><img src=' + img_uri + ' style="width:100%;margin-bottom:20px;border:1px solid #eee;" class="img_insta"></a></div>';
                     elem.append(object);
                     showImage();
                 }
