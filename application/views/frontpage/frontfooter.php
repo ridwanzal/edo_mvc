7bff2994b66e2352142df1c289b83ffced7d6c51
ridwanzal <zalbinaridwan@gmail.com>
2020-06-14 16:55:06 +0700

    compress / minified version

--- application/views/frontpage/frontfooter.php
+++ application/views/frontpage/frontfooter.php
@@ -27,7 +27,10 @@
 </script> 
     <script src="<?php echo base_url() ?>assets/lib/magnific-popups/dist/jquery.magnific-popup.min.js" type="text/javascript">
 </script> 
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+  crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </script> 
 <script src="<?php echo base_url() ?>assets/js/imageload.js" type="text/javascript">
