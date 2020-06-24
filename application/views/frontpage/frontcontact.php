27bf7d46d62776e08b85d1f8eb1f3956da33ea1f
ridwanzal <zalbinaridwan@gmail.com>
2020-06-24 18:10:26 +0700

    add all

--- application/views/frontpage/frontcontact.php
+++ application/views/frontpage/frontcontact.php
@@ -1,4 +1,4 @@
-<div class="thirdsection">
+<div class="thirdsection2">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-3 col-md-6 col-xs-12 container_images_me">
